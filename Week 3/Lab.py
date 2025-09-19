3977aff9ed48643e1ad5becbd8c9abbe0bab58b4
Nate17328 <natel6252020@gmail.com>
2025-09-19 08:45:46 -0500

    Lab 4

---
+++ Week 3/Lab.py
@@ -0,0 +1,9 @@
+light_color = input("What color is the light? ")
+if light_color == "green":
+    print("go")
+elif light_color == "yellow":
+    print("yeild")
+elif light_color == "red":
+    print("red")
+else:
+    print("Invalid")
